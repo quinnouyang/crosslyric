ef5a693c565e3d3d7da18a107085ea618d8131ee
quinnouyang <90884224+quinnouyang@users.noreply.github.com>
2024-02-25 06:40:38 -0600

    Instantiate crossword draft

---
+++ client/src/app/crossword.ts
@@ -0,0 +1,108 @@
+const data = {
+  across: {
+    "1": {
+      clue: [
+        "Where I'm at, I'm up where I'm at\n",
+        "___ ____ __? I'm done\n",
+        "You miss me? No, duh\n",
+      ],
+      answer: "YOUWANTME",
+      row: 0,
+      col: 0,
+    },
+    "3": {
+      answer: "SENDAPIC",
+      clue: [
+        "You're a waste of time, ahh",
+        "You fit every stereotype, ____ _ ___\n",
+        "I feel so much lighter like a feather with you out my life\n",
+      ],
+      row: 5,
+      col: 6,
+    },
+    "4": {
+      answer: "HOURSINADVANCE",
+      clue: [
+        "Then you pull back, when I try to make plans\n",
+        "More than two _____ __ _______, hmm\n",
+        "I slam the door, I hit ignore\n",
+      ],
+      row: 3,
+      col: 8,
+    },
+    "6": {
+      answer: "WHEREI'MAT",
+      clue: [
+        "You miss me? No, duh\n",
+        "_____ ___ __, I'm up where I'm at\n",
+        "You want me? I'm done\n",
+      ],
+      row: 8,
+      col: 0,
+    },
+  },
+  down: {
+    "2": {
+      clue: [
+        "More than two hours in advance, hmm",
+        "I ____ ___ ____, I hit ignore\n",
+        "I'm saying, no, no, no, no more\n",
+      ],
+      answer: "SLAMTHEDOOR\n",
+      row: 0,
+      col: 2,
+    },
+    "5": {
+      answer: "YOUWANTME",
+      clue: [
+        "Where I'm at, I'm up where I'm at\n",
+        "__ _____ __? I'm done\n",
+        "You miss me? No, duh\n",
+      ],
+      row: 0,
+      col: 0,
+    },
+    "7": {
+      answer: "WASTEOFTIME",
+      clue: [
+        "Floatin' through the memories like whatever\n",
+        "You're a _____ __ ____, ahh\n",
+        "You fit every stereotype, send a pic\n",
+      ],
+      row: 9,
+      col: 3,
+    },
+    "8": {
+      answer: "I'MDONE",
+      clue: [
+        "Like a feather, like a feather, like a feather\n",
+        "You want me? ___ ____\n",
+        "You miss me? No, duh\n",
+      ],
+      row: 12,
+      col: 4,
+    },
+    "9": {
+      answer: "WASTEOFTIME",
+      clue: [
+        "Floatin' through the memories like whatever\n",
+        "You're a _____ __ ____, ahh\n",
+        "Your signals are mixed, you act like a bitch\n",
+      ],
+      row: 4,
+      col: 0,
+    },
+    "10": {
+      answer: "CUTYOUROFF",
+      clue: [
+        "After this, an afterthought\n",
+        "I finally ___ ___ ___\n",
+        "I feel so much lighter like a feather with you off my mind, ahh\n",
+      ],
+      row: 3,
+      col: 6,
+    },
+  },
+};
+
+export default data;
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -2,6 +2,7 @@
 "use client";
 import { Link } from "@chakra-ui/next-js";
 import Crossword from "@jaredreisinger/react-crossword";
+import data from "./crossword";
 
 export default function Home() {
   return (
@@ -9,26 +10,7 @@ export default function Home() {
       <Link href="/play" color="blue.400" _hover={{ color: "blue.500" }}>
         Play
       </Link>
-      <Crossword
-        data={{
-          across: {
-            1: {
-              clue: "one plus one",
-              answer: "TWO",
-              row: 0,
-              col: 0,
-            },
-          },
-          down: {
-            2: {
-              clue: "three minus two",
-              answer: "ONE",
-              row: 0,
-              col: 2,
-            },
-          },
-        }}
-      />
+      <Crossword data={data} />
     </>
   );
 }
