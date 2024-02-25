39fb5d2d962a08c7a0ef822549c0bef7bd65ce2c
quinnouyang <90884224+quinnouyang@users.noreply.github.com>
2024-02-25 03:53:58 -0600

    Include audio information

--- server/audio.py
+++ server/audio.py
@@ -2,6 +2,7 @@ from yt_dlp import YoutubeDL
 
 OPTIONS = {
     "format": "wav/bestaudio/best",
+    "outtmpl": "./%(title)s.%(ext)s",
     "postprocessors": [
         {
             "key": "FFmpegExtractAudio",
@@ -11,6 +12,17 @@ OPTIONS = {
 }
 
 
-def download_audio(url: str) -> None:
+def download_audio(url: str) -> dict[str, str]:
     with YoutubeDL(OPTIONS) as ydl:
-        error_code = ydl.download([url])
+        info = ydl.extract_info(url)
+        assert info
+
+        return {
+            "artist": info["uploader"],
+            "title": info["title"],
+            "file": info["title"] + ".wav",
+        }
+
+
+if __name__ == "__main__":
+    print(download_audio("https://www.youtube.com/watch?v=asew9BF1wdw"))
